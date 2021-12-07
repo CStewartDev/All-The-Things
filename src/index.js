654592aacfe1805898be5334b269aa3f799ec4c2
Chris Stewart <c.stewart5545@gmail.com>
2021-12-06 20:29:19 -0600

    initial commit

---
+++ src/index.js
@@ -0,0 +1,10 @@
+import './styles.css';
+import { createTodo } from './todo';
+
+const boop = document.createElement('div');
+boop.append("I'm a boop!")
+boop.classList.add('boop-red');
+todo()
+
+
+document.body.appendChild(boop)
